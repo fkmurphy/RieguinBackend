07605985bb683350a9097563041651f91dcaa9eb
Julian Murphy <njmdistrisoft@gmail.com>
2020-08-26 23:05:35 -0300

    admin and data

--- app/gpios/admin.py
+++ app/gpios/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from gpio.models import Gpio
+
+#admin.site.register(Profile)
+@admin.register(Gpio)
+class GpioAdmin(admin.ModelAdmin):
+
+    list_display=('pin','enable')
