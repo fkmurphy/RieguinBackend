e4adb2f97959a9ea4fdd813e071c933f811403d9
Julian Murphy <njmdistrisoft@gmail.com>
2020-08-26 22:30:44 -0300

    fix

--- app/components/models/controllers.py
+++ app/components/models/controllers.py
@@ -1,5 +1,5 @@
 # Create your models here.
 from django.db import models
-
+from components.models.components import Component
 class Controller(Component):
         direction = models.CharField(max_length=12,default="out")
--- app/components/models/sensors.py
+++ app/components/models/sensors.py
@@ -1,3 +1,4 @@
 from django.db import models
+from components.models.components import Component
 class Sensor(Component):
         direction = models.CharField(max_length=12,default="in")
